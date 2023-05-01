06aeea8881929dd4dc7747b8bd4c499f4311090a
Rahulalvk <rahulal432@gmail.com>
2023-05-01 01:44:26 -0700

    Factors of a number

---
+++ Factor/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factor
+{
+    internal class Class1
+    { 
+        public static void DisplayFactors()
+        {
+            Console.WriteLine("Enter the Number to find the prime factors");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num); 
+            for (int i = 2; i <= num; i++)
+            {
+                
+                while(num%i== 0)
+                {
+                    Console.WriteLine("the fact is :{0}",i);
+                   
+                    i++;
+                }
+            }
+
+        }
+    }
+}
